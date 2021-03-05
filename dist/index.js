78c54d0a4483776e39f78089e5f733ed66cb623a
simple elpmis <simple@elpm.is>
2021-03-04 20:47:21 -0500

    generate month in mark down
    provide month and year

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,61 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.daysInMonth = void 0;
+exports.monthTable = exports.weekDay = exports.daysInMonth = void 0;
 function daysInMonth(month, year) {
-    var date = new Date(year, month, 0);
-    var days = date.getDate();
-    console.log(days);
+    var dateObj = new Date(year, month, 0);
+    var daysNum = dateObj.getDate();
+    return daysNum;
 }
 exports.daysInMonth = daysInMonth;
+function weekDay(month, year) {
+    var dateObj = new Date(year, month - 1, 1);
+    var weekDayNum = dateObj.getDay();
+    return weekDayNum;
+}
+exports.weekDay = weekDay;
+function monthTable(month, year) {
+    var daysNum = daysInMonth(month, year);
+    var weekDayNum = weekDay(month, year);
+    var breakLine = '\n|';
+    var monthTable = '|';
+    var weekDaysDone = 0;
+    var weekDaysRemain = 7;
+    var WeekDays;
+    (function (WeekDays) {
+        WeekDays[WeekDays["Sunday"] = 0] = "Sunday";
+        WeekDays[WeekDays["Monday"] = 1] = "Monday";
+        WeekDays[WeekDays["Tuesday"] = 2] = "Tuesday";
+        WeekDays[WeekDays["Wednesday"] = 3] = "Wednesday";
+        WeekDays[WeekDays["Thursday"] = 4] = "Thursday";
+        WeekDays[WeekDays["Friday"] = 5] = "Friday";
+        WeekDays[WeekDays["Saturday"] = 6] = "Saturday";
+    })(WeekDays || (WeekDays = {}));
+    for (var i = 0; i <= 6; i++) {
+        monthTable += WeekDays[i] + '|';
+    }
+    monthTable += breakLine;
+    for (var i = 0; i <= 6; i++) {
+        monthTable += ':---:|';
+    }
+    monthTable += breakLine;
+    for (var i = 0; i < (daysNum + weekDayNum); i++) {
+        if (i < weekDayNum) {
+            monthTable += '  |';
+        }
+        else {
+            monthTable += (i - weekDayNum + 1) + '|';
+        }
+        weekDaysDone = (i % 7);
+        if ((i != 0) && (weekDaysDone == 0)) {
+            monthTable += breakLine;
+        }
+    }
+    weekDaysRemain = weekDaysRemain - weekDaysDone;
+    while (weekDaysRemain > 0) {
+        monthTable += '  |';
+        weekDaysRemain--;
+    }
+    return monthTable.slice(0, -1);
+}
+exports.monthTable = monthTable;
 //# sourceMappingURL=index.js.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,58 @@
 export function daysInMonth(month: number, year: number) {
-    const date: Date = new Date(year, month,0)
-    const days: number = date.getDate()
-    console.log(days)
+    const dateObj: Date = new Date(year, month, 0)
+    const daysNum: number = dateObj.getDate()
+    return daysNum
+}
+
+export function weekDay(month: number, year: number) {
+    const dateObj: Date = new Date(year, month - 1, 1)
+    const weekDayNum: number = dateObj.getDay()
+    return weekDayNum
+}
+
+export function monthTable(month: number, year: number) {
+    const daysNum: number = daysInMonth(month, year)
+    const weekDayNum: number = weekDay(month, year)
+    const breakLine: string = '\n|'
+    let monthTable: string = '|'
+    let weekDaysDone: number = 0
+    let weekDaysRemain: number = 7
+    enum WeekDays {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+
+    for (let i: number = 0; i <= 6; i++) {
+        monthTable += WeekDays[i] + '|'
+    }
+    monthTable += breakLine
+
+    for (let i: number = 0; i <= 6; i++) {
+        monthTable += ':---:|'
+    }
+    monthTable += breakLine
+
+    for (let i: number = 0; i < (daysNum + weekDayNum); i++) {
+        if (i < weekDayNum) {
+            monthTable += '  |'
+        } else {
+            monthTable += (i - weekDayNum + 1) + '|'
+        }
+        weekDaysDone = (i % 7)
+        if ((i != 0) && (weekDaysDone == 0)) {
+            monthTable += breakLine
+        }
+    }
+
+    weekDaysRemain = weekDaysRemain - weekDaysDone
+    while (weekDaysRemain > 0) {
+        monthTable += '  |'
+        weekDaysRemain--;
+    }
+    return monthTable.slice(0, -1)
 }
