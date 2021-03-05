361c5a8305cf560140ad168e161ba2defd3b8d9d
simple elpmis <simple@elpm.is>
2021-03-05 18:48:41 -0500

    cli help

---
+++ bin/month-md.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const actions = process.argv.slice(2);
+
+function helpmehelpyou() {
+  console.log(`Usage:
+  month-md month year
+    ie., month-md 3 2021
+Options: 
+  -h, --help        Help menu`)
+}
+
+if ((actions.indexOf('--help') >= 0) || (actions.indexOf('-h') >= 0)) {
+  helpmehelpyou()
+  process.exit(0)
+} 
+
+if(actions.length = 2){
+  const monthMD = require('../dist/index')
+  const monthTable = monthMD.monthTable(actions[0],actions[1])
+  console.log(monthTable)
+}
+else{
+  console.log('Invalid,\n -h or --help for HELP')
+  process.exit(0)
+}
